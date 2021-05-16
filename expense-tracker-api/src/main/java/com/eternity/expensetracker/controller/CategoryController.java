84ef76c20bc490e956865187d20d0f1874fdd26d
Siddhesh Bhagat <siddheshbhagat@Siddheshs-MacBook-Pro.local>
2021-05-16 16:27:22 +0530

    Implemented get one category by Id

--- expense-tracker-api/src/main/java/com/eternity/expensetracker/controller/CategoryController.java
+++ expense-tracker-api/src/main/java/com/eternity/expensetracker/controller/CategoryController.java
@@ -3,11 +3,15 @@ package com.eternity.expensetracker.controller;
 import com.eternity.expensetracker.model.Category;
 import com.eternity.expensetracker.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -23,6 +27,13 @@ public class CategoryController {
     @GetMapping("/categories")
     Collection<Category> getAllCatergories() {
         return categoryRepository.findAll();
+    }
+
+    @GetMapping("/category/{id}")
+    ResponseEntity<?> getCategory(@PathVariable Long id) {
+        Optional<Category> category = categoryRepository.findById(id);
+        return category.map(response -> ResponseEntity.ok().body(response))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
 }
